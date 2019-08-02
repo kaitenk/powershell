d7084a5d83bdc563d0c6fa0836f00e8c12db9093
kaitenk <kala.kaiten@gmail.com>
2019-08-02 02:31:54 -0400

    First commit, near damn perfect

---
+++ ActiveRAMUsage.ps1
@@ -0,0 +1,18 @@
+#Requires -RunAsAdministrator
+
+Import-Module -Name "Hyper-V"
+
+Write-Host "Current RAM usage running VMs"
+
+$currentRAMUsageVM = Get-VM -VMName "*" -ComputerName jager | Where-Object -Property State -EQ "Running" | Select-Object VMName, @{Name="MemoryAssigned";Expression = {$_.MemoryAssigned/1mb}}, @{Name="MemoryDemand";Expression = {$_.MemoryDemand/1mb}}, MemoryStatus
+
+Out-Host -InputObject $currentRAMUsageVM
+
+Write-Host "Current RAM usage of hypervisor"
+
+$OS = Get-CimInstance Win32_OperatingSystem -ComputerName jager
+$totalMemory = $os.TotalVisibleMemorySize/1mb
+$freeMemory = $os.FreePhysicalMemory/1mb
+$usedMemory = $totalMemory-$freeMemory
+
+Write-Host $usedMemory.ToString("#.#")  "/"  $totalMemory.ToString("#.#")GB
