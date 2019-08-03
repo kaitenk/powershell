a6acae57ebfe71541a86bb4806e80a4c74192d35
kaitenk <kala.kaiten@gmail.com>
2019-08-02 22:33:56 -0400

    Removed the old code

--- ActiveVMCoreCount.ps1
+++ ActiveVMCoreCount.ps1
@@ -2,15 +2,10 @@
 
 Import-Module -Name "Hyper-V"
 
-# Write-Host "Running VMs"
-
-# Get-VM -VMName "*" -ComputerName jager| Where-Object -Property State -EQ "Running" | Format-Table Name
 $running = Get-VM -VMName "*" -ComputerName jager | Where-Object -Property State -EQ "Running" | Select-Object -ExpandProperty Name
 
 Write-Host "Current vCPU core count of running VMs"
 
 ForEach ($vm in $running) {
     Get-VMProcessor -VMName $vm -ComputerName jager | Select-Object -Property VMName, Count
-    # $runningCPUCount = Get-VMProcessor -VMName $vm -ComputerName jager
-    # Write-Host $runningCPUCounts | Format-Table VMName, Count
 }
