9e61708a8681b5feda5c134836bb241fc52f734c
kaitenk <kala.kaiten@gmail.com>
2019-08-17 00:47:17 -0400

    Back to 5 seconds

--- HypervisorReportingWrapper.ps1
+++ HypervisorReportingWrapper.ps1
@@ -7,8 +7,8 @@ Start-Sleep -Seconds 2
 
 .\ActiveRAMUsage.ps1
 Clear-Variable running* -Scope Global
-Write-Host "Sleeping for 2 seconds"
-Start-Sleep -Seconds 2
+Write-Host "Sleeping for 5 seconds"
+Start-Sleep -Seconds 5
 
 .\ActiveVMCoreCount.ps1
 
