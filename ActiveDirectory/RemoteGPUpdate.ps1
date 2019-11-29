dbe4fbb5c74cd9309f6f9aa3a7ba3c6d8558162f
kaitenk <kala.kaiten@gmail.com>
2019-11-29 14:46:42 -0500

    Reverted to individual computers at a time

--- ActiveDirectory/RemoteGPUpdate.ps1
+++ ActiveDirectory/RemoteGPUpdate.ps1
@@ -1,7 +1,5 @@
 $computers = Get-ADComputer -Filter * | Select-Object -ExpandProperty "Name" 
-# foreach ($computer in $computers) {
-#     Invoke-GPUpdate -Force -Computer $computer -Verbose -ErrorAction SilentlyContinue -AsJob
-#     Invoke-Command -ComputerName $computer -ScriptBlock {hostname} -ErrorAction SilentlyContinue
-# }
 
-Invoke-AsWorkflow -PSComputerName $computers -CommandName Invoke-GPUpdate
+Invoke-Command -AsJob -ComputerName $computers -ScriptBlock {
+    Invoke-GPUpdate -Force
+}
