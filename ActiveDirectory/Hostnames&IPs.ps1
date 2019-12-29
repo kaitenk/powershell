124b800797fd88397a44ab515b7af88379e049a3
kaitenk <kala.kaiten@gmail.com>
2019-12-29 00:00:50 -0500

    Removed extra output to console and restricted to IPv4

--- ActiveDirectory/Hostnames&IPs.ps1
+++ ActiveDirectory/Hostnames&IPs.ps1
@@ -1,8 +1,10 @@
-Get-Module ActiveDirectory
+#Requires -Module ActiveDirectory
+ 
+$OU = Get-ADOrganizationalUnit -Filter 'Name -like "Domain Computers"'
 
 $hostNames = Get-ADComputer -Filter * -SearchBase "OU=Domain Computers, DC=kkala, DC=internal" | Select-Object -ExpandProperty "Name"
 
 foreach ($computer in $hostNames) {
-    $ip = Resolve-DnsName -Name $computer | Select-Object -ExpandProperty IP4Address 
+    $ip = Resolve-DnsName -Name $computer -Type A | Select-Object -ExpandProperty IP4Address -ErrorAction Inquire
     Write-Host $computer, $ip 
 }
