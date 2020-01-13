d2adcfabab44c4d2244056279f1f9e4b8b8c3059
kaitenk <kala.kaiten@gmail.com>
2020-01-13 18:59:51 -0500

    Removed unecessary -SearchBase

--- ActiveDirectory/Hostnames&IPs.ps1
+++ ActiveDirectory/Hostnames&IPs.ps1
@@ -1,6 +1,6 @@
 #Requires -Module ActiveDirectory
 
-$hostNames = Get-ADComputer -Filter * -SearchBase "OU=Domain Computers, DC=kkala, DC=internal" | Select-Object -ExpandProperty "Name"
+$hostNames = Get-ADComputer -Filter * | Select-Object -ExpandProperty "Name"
 
 foreach ($computer in $hostNames) {
     $ip = Resolve-DnsName -Name $computer -Type A | Select-Object -ExpandProperty IP4Address -ErrorAction Inquire
