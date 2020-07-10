eb8ff1227946ac9388e3d9809973500ff98921bf
kaitenk <kala.kaiten@gmail.com>
2020-07-10 19:40:12 -0400

    GUI prompts user to select folder where VMs are

--- HypervisorReporting/VMStorageUsed.ps1
+++ HypervisorReporting/VMStorageUsed.ps1
@@ -1,17 +1,20 @@
-#Requires -RunAsAdministrator
-
 Import-Module -Name "Hyper-V"
 
 Write-Host "Current usage of VM storage"
 
-Invoke-Command -ScriptBlock {
-    $vmStorage = Get-PSDrive -Name V
+Add-Type -AssemblyName System.Windows.Forms
 
-    $vmStorageFree = $vmStorage.Free/1gb
-    $vmStorageUsed = $vmStorage.Used/1gb
-    $vmStorageTotal = $vmStorageFree+$vmStorageUsed
+$fileBrowser = New-Object System.Windows.Forms.FolderBrowserDialog 
 
-    Write-Host $vmStorageFree.ToString("#.#")  "/"  $vmStorageTotal.ToString("#.#")GB
+$fileBrowser.ShowNewFolderButton = $false
+$fileBrowser.Description = "Select the directory containing Hyper-V VMs"
+[void]$fileBrowser.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true}))
+ 
+# $vmStorage = Get-PSDrive -PSProvider FileSystem | Where-Object -FilterScript { $_.Name -NE "C" -and $_.Root -notlike '\\*' }
+
+foreach ($disk in $vmStorage) {
+   $diskSize = [Math]::Truncate($disk.Size/1gb)
+   $diskUsed = Get-PSDrive -Name (Get-Volume -FilePath $disk.Path)
 }
 
 $running = Get-VM | Where-Object -Property State -EQ "Running" | Select-Object -ExpandProperty Name
