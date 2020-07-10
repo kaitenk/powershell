c9965e22aec8fc1cb17ba530c9b5f1627900677d
kaitenk <kala.kaiten@gmail.com>
2020-07-10 19:38:42 -0400

    Tell host keystroke to end command.

--- ActiveDirectory/PingTest.ps1
+++ ActiveDirectory/PingTest.ps1
@@ -6,10 +6,11 @@ $reach = $true
 do {
     if ((Test-Connection -ComputerName $hostname -Quiet) -eq $true) {
         Write-Host "$hostname is reachable"
+        Write-Host "Press CTRL+C to stop"
     }
     else {
-        [console]::beep(500,100) 
+        [console]::beep(500,100)
+        Write-Host "Press CTRL+C to stop" 
     }
 } 
 while ($reach = $true)
-
